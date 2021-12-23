498554c4959dcdd37000c7600ad202345f7eb4f9
Bernardolgm99 <bernardolgm99@gmail.com>
2021-12-23 15:23:19 +0000

    nova versão do menu, opcao de que jogador comeca a jogar menu apos modos de jogo

--- tic-tac-toeV2.py
+++ tic-tac-toeV2.py
@@ -9,7 +9,7 @@ def WinningDetection(gameStatus, round):
         if round%2 == 0:                                                #Verifica se o jogador 1 ganhou
             player1 = []
             for i in range(9):                                          #Verifica onde o jogador 1 já marcou
-                if gameStatus[i] == "O":
+                if gameStatus[i] == "OO":
                     player1.append(i)
             for i in range(len(winningPossibilities)):
                 winPlayer1 = 0
@@ -21,7 +21,7 @@ def WinningDetection(gameStatus, round):
         else:                                                           #Verifica se o jogador 2 ganhou
             player2 = []
             for i in range(9):
-                if gameStatus[i] == "X":                                #Verifica onde o jogador 2 já marcou
+                if gameStatus[i] == "XX":                                #Verifica onde o jogador 2 já marcou
                     player2.append(i)
             for i in range(len(winningPossibilities)):
                 winPlayer2 = 0
@@ -39,10 +39,10 @@ def Board(gameStatus):
 # Marca a posição selecionada pelo jogador
 def SelectPosition(gameStatus, player, numberSelected):
     if player == 1:
-        gameStatus.insert(numberSelected-1,"X") # troca a posição numerica por "X"
+        gameStatus.insert(numberSelected-1,"XX") # troca a posição numerica por "XX"
         del gameStatus[numberSelected]
     elif player == 0:
-        gameStatus.insert(numberSelected-1,"O") # troca a posição numerica por "O"
+        gameStatus.insert(numberSelected-1,"OO") # troca a 1posição numerica por "OO"
         del gameStatus[numberSelected]
     return gameStatus
 
@@ -58,18 +58,18 @@ def AI(gameStatus,round):
     winningPossibilities2 = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]]
     player1 = []
     player2 = []
-    # Marca a situação de jogo do jogador "O" e jogador "X" em forma de motrar suas posições
+    # Marca a situação de jogo do jogador "OO" e jogador "XX" em forma de motrar suas posições
     if round%2 != 0:
         for i in range(len(gameStatus)):
-            if gameStatus[i] == "O":
+            if gameStatus[i] == "OO":
                 player1.append(i)
-            if gameStatus[i] == "X":
+            if gameStatus[i] == "XX":
                 player2.append(i)
     else:
         for i in range(len(gameStatus)):
-            if gameStatus[i] == "X":
+            if gameStatus[i] == "XX":
                 player1.append(i)
-            if gameStatus[i] == "O":
+            if gameStatus[i] == "OO":
                 player2.append(i)
 
     # IA tenta ganhar
@@ -111,186 +111,205 @@ def AI(gameStatus,round):
 
 # Modo de jogo contra a IA
 def AIGame():
-    gameStatus = ["1","2","3","4","5","6","7","8","9"] # posição das casas do jogo
-    players=["AI"]
-    flipCoin = random.randint(0,1)
-    # Adiciona o jogador ao jogo
-    players.insert(flipCoin,(input("Player name: ")))
-
-    
-    os.system("cls")
-    print ("Select a number from the board: ")
-
-    # While onde acontece o jogo
-    round=0
-    while round != 9:
-        turn = 0
-        if round%2 == 0:
-            turn = players[0]
-        else:
-            turn = players[1]
-        Board(gameStatus) # Print do tabuleiro
-        try:
-            # Vez jogador
-            if turn != "AI":
-                numberSelected = int(input("Round " + str(round+1) + "\n" + players[round%2] + "'s turn:")) # Jogador seleciona onde quer jogar
-                if str(numberSelected) not in gameStatus and  numberSelected < 10:
-                    round-=1
+    while True:
+        # Adiciona o jogador ao jogo
+        playerName = input("Player name: ")
+        os.system("cls")
+        # While onde acontece o jogo
+        while True:
+            players=[playerName,"AI"]
+            while True:
+                selectPlayer = input("Which player goes first?\n1 - %s\n2 - %s\n3 - Random\n\n" %(players[0], players[1]))
+                if selectPlayer in ["1","2","3"]:
+                    break
+                else:
                     os.system("cls")
-                    print("It's aready selected")
-                elif numberSelected > 10:
+                    print("Select a valid input, pls!!!")
+            if selectPlayer == "3":
+                selectPlayer = random.randint(1,2)
+            if int(selectPlayer) == 2:
+                players.reverse()
+            os.system("cls")
+            gameStatus = ["1","2","3","4","5","6","7","8","9"] # posição das casas do jogo
+            round=0
+            print ("Select a number from the board: ")
+            while round != 9:
+                turn = 0
+                if round%2 == 0:
+                    turn = players[0]
+                else:
+                    turn = players[1]
+                Board(gameStatus) # Print do tabuleiro
+                try:
+                    # Vez jogador
+                    if turn != "AI":
+                        numberSelected = int(input("Round " + str(round+1) + "\n" + players[round%2] + "'s turn:")) # Jogador seleciona onde quer jogar
+                        if str(numberSelected) not in gameStatus and  numberSelected < 10:
+                            round-=1
+                            os.system("cls")
+                            print("It's aready selected")
+                        elif numberSelected > 10:
+                            round-=1
+                            os.system("cls")
+                            print("Invalid value")
+                        else:
+                            gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
+
+                            winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
+                            os.system("cls")
+                            if winner == 0 or winner == 1:
+                                Board(gameStatus)
+                                print("%s wins!!!" %(players[winner]))
+                                break
+                            if round == 8:
+                                Board(gameStatus)
+                                print ("Draw")
+                    else:
+                    #Vez IA
+                        numberSelected = AI(gameStatus,round) # IA toma decisão de valor que irá jogar
+                        gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
+
+                        winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
+                        os.system("cls")
+                    
+                        if winner == 0 or winner == 1:
+                            Board(gameStatus)
+                            print("%s wins!!!" %(players[winner]))
+                            break
+                        if round == 8:
+                                Board(gameStatus)
+                                print ("Draw")
+                except ValueError:
+                    print("Invalid value")
                     round-=1
+                except:
+                    print("Something is wrong!")
+                round+=1
+            while True:
+                menuVsGame = input("1 - Play again\n2 - Change player's name\n3 - Back to menu\n\n")
+                if menuVsGame in ["1","2","3"]:
+                    break
+                else:
                     os.system("cls")
                     print("Invalid value")
-                else:
-                    gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
-
-                    winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
-                    os.system("cls")
-                    if winner == 0 or winner == 1:
-                        Board(gameStatus)
-                        print("%s wins!!!" %(players[winner]))
-                        break
-                    if round == 8:
-                        Board(gameStatus)
-                        print ("Draw")
-            else:
-            #Vez IA
-                numberSelected = AI(gameStatus,round) # IA toma decisão de valor que irá jogar
-                gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
-
-                winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
-                os.system("cls")
-            
-                if winner == 0 or winner == 1:
-                    Board(gameStatus)
-                    print("%s wins!!!" %(players[winner]))
-                    break
-                if round == 8:
-                        Board(gameStatus)
-                        print ("Draw")
-        except ValueError:
-            print("Invalid value")
-            round-=1
-        except:
-            print("Something is wrong!")
-        round+=1
-
+            os.system("cls")
+            if menuVsGame == "2":
+                break
+            elif menuVsGame == "3":
+                return "Exiting game mode"
 
 # Modo de jogo jogador vs jogador
 def VsGame():
-    gameStatus = ["1","2","3","4","5","6","7","8","9"] # posição das casas do jogo
-    players=[]
-    # Adiciona os jogadores ao jogo
-    for i in range(2):
-        players.append(input("Name of player " + str(i+1) + ": "))
-    os.system("cls")
+    menuVsGame = 0
+    #Onde inicia o modo de jogo
     while True:
-        selectPlayer = input("Which player goes first?\n1 - %s\n2 - %s\n" %(players[0], players[1]))
-        if selectPlayer in ["1","2"]:
+        players=[]
+        # Adiciona os jogadores ao jogo
+        for i in range(2):
+            players.append(input("Name of player " + str(i+1) + ": "))
+        os.system("cls")
+        #Onde inicia o jogo
+        while True:
+            while True:
+                selectPlayer = input("Which player goes first?\n1 - %s\n2 - %s\n3 - Random" %(players[0], players[1]))
+                if selectPlayer in ["1","2","3"]:
+                    break
+                else:
+                    os.system("cls")
+                    print("Select a valid input, pls!!!")
+            if selectPlayer == "3":
+                selectPlayer = random.randint(1,2)
+            if int(selectPlayer) == 2:
+                players.reverse()
+            os.system("cls")
+            # While onde acontece o jogo
+            round=0
+            gameStatus = ["1","2","3","4","5","6","7","8","9"] # posição das casas do jogo
+            print ("Select a number from the board")
+            while round != 9:
+                Board(gameStatus) #Print do mapa
+                try:
+                    # Vez jogador "OO"
+                    if round%2 == 0:
+                        numberSelected = int(input("Round " + str(round+1) + "\n" + players[round%2] + "'s turn:")) # Jogador seleciona onde quer jogar
+                        if str(numberSelected) not in gameStatus and  numberSelected < 10:
+                            round-=1
+                            os.system("cls")
+                            print("It's aready selected")
+                        elif numberSelected > 10:
+                            round-=1
+                            os.system("cls")
+                            print("Invalid value")
+                        else:
+                            gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
+
+                            winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
+                            os.system("cls")
+                            if winner == 0 or winner == 1:
+                                Board(gameStatus)
+                                print("%s wins!!!" %(players[winner]))
+                                break
+                            if round == 8:
+                                Board(gameStatus)
+                                print ("Draw")
+                    else:
+                    # Vez jogador "XX"
+                        numberSelected = int(input("Round " + str(round+1) + "\n" + players[round%2] + "'s turn:")) # Jogador seleciona onde quer jogar
+                        if str(numberSelected) not in gameStatus and  numberSelected < 10:
+                            round-=1
+                            os.system("cls")
+                            print("It's aready selected")
+                        elif numberSelected > 10:
+                            round-=1
+                            os.system("cls")
+                            print("Invalid value")
+                        else:
+                            gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
+
+                            winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
+                            os.system("cls")
+                            if winner == 0 or winner == 1:
+                                Board(gameStatus)
+                                print("%s wins!!!" %(players[winner]))
+                                break
+                except ValueError:
+                    print("Invalid value")
+                    round-=1
+                except:
+                    print("Something is wrong!")
+                round+=1
+            input("Press to continue...")
+            os.system("cls")
+            while True:
+                menuVsGame = input("1 - Play again\n2 - Change player's name\n3 - Back to menu\n\n")
+                if menuVsGame in ["1","2","3"]:
+                    break
+                else:
+                    os.system("cls")
+                    print("Invalid value")
+            os.system("cls")
+            if menuVsGame == "2":
+                break
+            elif menuVsGame == "3":
+                return "Exiting game mode"
+
+def Main():
+    while True:
+        print("MENU\n")
+        modeGame = input("1 - Singleplayer\n2 - Multiplayer\n3 - Quit\n\nSelect game mode:")
+        if modeGame == "1":
+            os.system("cls")
+            print("AI game mode!")
+            AIGame()
+        elif modeGame == "2":
+            os.system("cls")
+            print("Vs game mode!")
+            VsGame()
+        elif modeGame == "3":
             break
         else:
             os.system("cls")
-            print("Select a valid input, pls!!!")
-    if selectPlayer == "2":
-        players.reverse()
-    os.system("cls")
-    print ("Select a number from the board")
-
-    # While onde acontece o jogo
-    round=0
-    while round != 9:
-        Board(gameStatus) #Print do mapa
-        try:
-            # Vez jogador "O"
-            if round%2 == 0:
-                numberSelected = int(input("Round " + str(round+1) + "\n" + players[round%2] + "'s turn:")) # Jogador seleciona onde quer jogar
-                if str(numberSelected) not in gameStatus and  numberSelected < 10:
-                    round-=1
-                    os.system("cls")
-                    print("It's aready selected")
-                elif numberSelected > 10:
-                    round-=1
-                    os.system("cls")
-                    print("Invalid value")
-                else:
-                    gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
-
-                    winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
-                    os.system("cls")
-                    if winner == 0 or winner == 1:
-                        Board(gameStatus)
-                        print("%s wins!!!" %(players[winner]))
-                        break
-                    if round == 8:
-                        Board(gameStatus)
-                        print ("Draw")
-            else:
-            # Vez jogador "X"
-                numberSelected = int(input("Round " + str(round+1) + "\n" + players[round%2] + "'s turn:")) # Jogador seleciona onde quer jogar
-                if str(numberSelected) not in gameStatus and  numberSelected < 10:
-                    round-=1
-                    os.system("cls")
-                    print("It's aready selected")
-                elif numberSelected > 10:
-                    round-=1
-                    os.system("cls")
-                    print("Invalid value")
-                else:
-                    gameStatus = SelectPosition(gameStatus, round%2, numberSelected) # Atuzaliza a variavel gameStatus para as novas definições escolhidas pelo usuario
-
-                    winner = WinningDetection(gameStatus, round) # Verifica se teve um vencerdor
-                    os.system("cls")
-                    if winner == 0 or winner == 1:
-                        Board(gameStatus)
-                        print("%s wins!!!" %(players[winner]))
-                        break
-        except ValueError:
-            print("Invalid value")
-            round-=1
-        except:
-            print("Something is wrong!")
-        round+=1
-
-def Main():
-    playAgain = True
-    askPlayAgain = ""
-    while playAgain != False:
-        try:
-            playAgainGameMode = True
-            if askPlayAgain == "":
-                selectMode = int(input("Which game mode do you wish to play?\n\n1 - Singleplayer\n2 - Multiplayer\n"))
-            if selectMode == 1:
-                while playAgainGameMode != False:
-                    os.system("cls")
-                    print("AI game mode!")
-                    AIGame()
-                    askPlayAgainGameMode = input("Do you want to play vs AI again? (Y/N):")
-                    if askPlayAgainGameMode.upper() != "Y":
-                        playAgainGameMode = False
-            elif selectMode == 2:
-                while playAgainGameMode != False:
-                    os.system("cls")
-                    print("Vs game mode!")
-                    VsGame()
-                    askPlayAgainGameMode = input("Do you want to play vs another player again? (Y/N):")
-                    if askPlayAgainGameMode.upper() != "Y":
-                        playAgainGameMode = False
-            os.system("cls")  
-            askPlayAgain = input("Do you want to play Tic-Tac-Toe again? (Y/N):")
-            if askPlayAgain.upper() != "Y":
-                playAgain = False
-            else:
-                if selectMode == 1:
-                    selectMode = 2
-                elif selectMode == 2:
-                    selectMode = 1
-        except ValueError:
-            os.system("cls")
             print("Invalid value. Try again!")
-        except:
-            os.system("cls")
-            print("Something is wrong!")
 
 os.system("cls")
 Main()
